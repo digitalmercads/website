b2a51a6d484d59127757292454bd8e4ec6a0884a
digitalmercads <digital.mercads@mail.com>
2020-08-29 17:30:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="es_MX">
+
 <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>MercAds - Estrategas Digitales</title>
+
+
+
+
 </head>
+
 <body>
     <h1>Hola Git :)</h1>
 </body>
+
 </html>
